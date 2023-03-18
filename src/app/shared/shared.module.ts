a87ecc797c2e468c7bc8cd51c07b42313c00d3aa
Hala Farag <halaafaragg@gmail.com>
2023-03-18 19:35:16 +0200

    footer & card

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,10 +4,24 @@ import { HeaderComponent } from './components/header/header.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { NgxHideOnScrollModule } from 'ngx-hide-on-scroll';
 import { SliderComponent } from './components/slider/slider.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
-  declarations: [HeaderComponent, NavbarComponent, SliderComponent],
+  declarations: [
+    HeaderComponent,
+    NavbarComponent,
+    SliderComponent,
+    FooterComponent,
+    HomeComponent,
+  ],
   imports: [CommonModule, NgxHideOnScrollModule],
-  exports: [HeaderComponent, NavbarComponent, SliderComponent],
+  exports: [
+    HeaderComponent,
+    NavbarComponent,
+    SliderComponent,
+    FooterComponent,
+    HomeComponent,
+  ],
 })
 export class SharedModule {}
