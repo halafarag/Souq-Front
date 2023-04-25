cc007451032b66638b74b49806eed49387eaf098
Hala Farag <halaafaragg@gmail.com>
2023-04-25 22:03:19 +0200

    deploy

--- src/enviroment/Enviroment.ts
+++
@@ -1,4 +0,0 @@
-export const enviroment = {
-  production: false,
-  apiBaseUrl: 'https://souq-back-end.vercel.app/',
-};
--- src/enviroment/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const enviroment = {
-  production: true,
-  apiBaseUrl: 'https://online-pharmacy-self.vercel.app',
-};
